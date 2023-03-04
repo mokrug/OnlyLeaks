08f9334afe0ffa4842d124122397f2b4c94e8bdd
Moritz Krug <krugmoritz@icloud.com>
2023-03-04 21:53:57 +0100

    ff

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const ua = require('universal-analytics');
-const { Helmet } = require('react-helmet');
 
 // Set up Google Analytics.js
 const visitor = ua('G-RY7JQ1HM4T');
@@ -13,14 +12,7 @@ app.use(function(req, res, next) {
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  // Set SEO metadata using react-helmet
-  const helmet = Helmet.renderStatic();
-  const seo = {
-    title: 'Free Premium OnlyFans Leaks',
-    description: 'Get access to free premium OnlyFans content. Join our Telegram and Discord groups to get access to leaked content.',
-    keywords: 'OnlyFans, leaks, premium content, free, Telegram, Discord, content, exclusive content, adult content, NSFW, sexy, erotic'
-  };
-  res.render('index', { helmet, seo });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function() {
