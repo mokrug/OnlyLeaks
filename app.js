c514b7749657859037fa8dd5a328cee4b6386516
Moritz Krug <krugmoritz@icloud.com>
2023-03-04 21:06:26 +0100

    fff

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
 const express = require('express');
-const sitemap = require('sitemap');
-const helmet = require('helmet');
-const prerender = require('prerender-node');
-
 const app = express();
 
-// Set up sitemap
-app.use(sitemap({
-  sitemap: 'sitemap.xml',
-  map: {
-    '/': ['get'],
-  },
-}));
+app.use(express.static('public'));
 
-// Set up SEO metadata middleware
-app.use(function(req, res, next) {
-  res.locals.seo = {
-    title: 'Free Premium OnlyFans Leaks',
-    description: 'Get access to free premium OnlyFans content. Join our Telegram and Discord groups to get access to leaked content.',
-    keywords: 'OnlyFans, leaks, premium content, free, Telegram, Discord, content, exclusive content, adult content, NSFW, sexy, erotic',
-    canonical: 'https://example.com/'
-  };
-  next();
-});
-
-// Set up Helmet.js
-app.use(helmet());
-
-// Set up Prerender.js
-app.use(prerender());
-
-// Set up routes
-app.get('/', (req, res) => {
-  res.send('Homepage');
-});
-
-// Start server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
