ac9a007fc6c4211e03194663280233e6841e2d60
Moritz Krug <krugmoritz@icloud.com>
2023-03-04 21:31:48 +0100

    index.html

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.use(function(req, res, next) {
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 app.listen(3000, function() {
   console.log('Server started on port 3000');
