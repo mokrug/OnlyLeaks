075345264be6e9bc8871d08e65592f2b4fa63a61
Moritz Krug <krugmoritz@icloud.com>
2023-03-04 21:23:46 +0100

    npm install

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const ua = require('universal-analytics');
+
+// Set up Google Analytics.js
+const visitor = ua('G-RY7JQ1HM4T');
+app.use(function(req, res, next) {
+  res.analytics = visitor;
+  next();
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function() {
   console.log('Server started on port 3000');
 });
