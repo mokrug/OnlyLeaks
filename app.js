809821ee36cc67258368a164f1444ae7ddfde14f
Moritz Krug <krugmoritz@icloud.com>
2023-03-04 21:34:38 +0100

    port

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3000, function() {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server started on port ' + (process.env.PORT || 3000));
 });
